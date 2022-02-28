c195e4598bf13fa1b228ba72bd982a939e39fa24
leejos5 <joshuajungminlee@gmail.com>
2022-02-28 15:13:36 -0800

    Initialized php and config files.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+ define('DBHOST', 'localhost');
+ define('DBNAME', 'unseen_library');
+ define('DBUSER', 'testuser');
+ define('DBPASS', 'mypassword');
+ define('DBCONNSTRING','mysql:dbname=unseen_library;charset=utf8mb4');
+?>
