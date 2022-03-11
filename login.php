3fb83c8fefc8c14f8de3b9370037971d7b0f227b
rinpham217 <rinpham1011@gmail.com>
2022-03-11 08:29:46 -0800

    sign up

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="login.php">Sign In
-          	<span class="sr-only">(current)</span>
+          	<span class="sr-only"></span>
 					</a>
         </li>
 				<li class="nav-item">
@@ -45,17 +45,14 @@
 		<h1>Login and Signup</h1>
 		<hr />
 		<div>
-			<?php
-			if (isset($_POST['register'])) {
-				echo 'User submmited.';
-			}
 
-			?>
 		</div>
 		<div id="illustrations-container">
 			<div class="card border-primary mb-3">
 	  		<div class="card-header">Login</div>
-				<fieldset>
+
+			  <form action = "signup.php" method ="post">
+					<fieldset>
 					<div class="form-group">
 						<label for="user-name" class="form-label mt-4">Username</label>
 						<input type="user-name" class="form-control" id="user-name" aria-describedby="usernameHelp" placeholder="Enter username">
@@ -65,28 +62,41 @@
 						<input type="password" class="form-control" id="password" placeholder="Password">
 					</div>
 					<br />
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</fieldset>
+					<button type="submit" class="btn btn-primary">Login</button>
+					</fieldset>
+				</form>
 			</div>
 			<div class="card border-primary mb-3">
 	  		<div class="card-header">Sign-Up</div>
-				<form action = "login.php" method ="post">
+				<form action = "signup.php" method ="post">
 				  <fieldset>
 				    <div class="form-group">
 				      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-				      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+				      <input type="email" class="form-control" name = "email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 				      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 				    </div>
-						<div class="form-group">
+					<div class="form-group">
 						 <label for="signupUsername" class="form-label mt-4">Username</label>
-						 <input type="signupUsername" class="form-control" id="signupUsername" placeholder="Username">
-					 </div>
-				    <div class="form-group">
+						 <input type="signupUsername" class="form-control" name ="user_name" id="signupUsername" placeholder="Username">
+					</div>
+					<div class="form-group">
+						 <label for="signupFirstname" class="form-label mt-4">First_name</label>
+						 <input type="signupFirstname" class="form-control" name = "first_name" id="signupFirstname" placeholder="First_name">
+					</div>
+					<div class="form-group">
+						 <label for="signupLastname" class="form-label mt-4">Last_name</label>
+						 <input type="signupLastname" class="form-control" name = "last_name" id="signupLastname" placeholder="Last_name">
+					</div>
+					<div class="form-group">
+						 <label for="signupPhone" class="form-label mt-4">Phone</label>
+						 <input type="signupPhone" class="form-control" name = "phone" id="signupPhone" placeholder="Phone">
+					</div>
+					<div class="form-group">
 				      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-				      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				      <input type="password" class="form-control" name ="password" id="exampleInputPassword1" placeholder="Password">
 				    </div>
 						<br />
-						<button type="submit" name = "register" class="btn btn-primary">Submit</button>
+						<button type="submit" name = "register" class="btn btn-primary">Sign up</button>
 					</fieldset>
 				</form>
 			</div>
---
+++ signup.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "testuser";
+$password = "mypassword";
+$dbname = "unseen_library";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST["register"])) {
+   
+    $username   = $_POST['user_name'];
+    $First_name = $_POST['first_name'];
+    $Last_name  = $_POST['last_name']; 
+    $email      = $_POST['email']; 
+    $phone      = $_POST['phone'];
+    $password   = $_POST['password'];
+    
+    $sql ="INSERT INTO `users` (`user_name`, `first_name`, `last_name`, `email`, `phone`, `password`) 
+    VALUES ('$username','$First_name','$Last_name','$email','$phone','$password')";
+    //$stmtinsert = $db ->prepare($sql);
+    //$result = $stmtinsert->execute([$username, $First_name, $Last_name, $email, $phone, $password])
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+$conn->close();
+?>
+
